458c92b12c06900f003847c0a838eb38342d5627
kwu849 <kwu849@aucklanduni.ac.nz>
2018-09-17 14:12:03 +1200

    GUI created

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("NameSayer.fxml"));
         primaryStage.setTitle("NameSayer");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 750, 500));
         primaryStage.show();
     }
 
