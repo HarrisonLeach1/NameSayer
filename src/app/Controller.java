3967c9d1110686ace48d10f7d6930cb086f5a474
Harrison Leach <HarrisonL7190@gmail.com>
2018-09-17 17:45:27 +1200

    Updated view and controller to use the treeView model

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -5,6 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
 import java.net.URL;
 import java.util.Observable;
@@ -13,15 +15,20 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    private ListView<String> listView;
+    private TreeView<String> _dataList;
 
     IDataModel dataModel = new DataModel();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        listView.getItems().addAll(dataModel.loadData());
+         _dataList.setRoot(dataModel.getTreeRoot());
+         _dataList.setShowRoot(false);
+     }
+
+    private TreeItem<String> addBranch(String name, TreeItem<String> parent) {
+        TreeItem<String> item = new TreeItem<>(name);
+        parent.getChildren().add(item);
+        return item;
     }
-
-
 
 }
