4fa79756836cd816993b4a2517d119aa6170a481
Harrison Leach <HarrisonL7190@gmail.com>
2018-09-22 10:58:09 +1200

    Improved play scene controller documentation

--- src/app/controllers/PlaySceneController.java
+++ src/app/controllers/PlaySceneController.java
@@ -1,9 +1,6 @@
 package app.controllers;
 
 import app.model.IPractiseListModel;
-import app.model.Name;
-import app.model.PractiseListModel;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,12 +15,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Observable;
 import java.util.ResourceBundle;
 
 public class PlaySceneController implements Initializable {
 
-    @FXML private Button play_btn,next_btn,bad_btn,return_btn,keep_btn, compare_btn;
+    @FXML private Button keep_btn, compare_btn;
     @FXML private Label _displayName;
 
     private IPractiseListModel _practiseListModel;
@@ -33,11 +29,20 @@ public class PlaySceneController implements Initializable {
 
     }
 
+    /**
+     * Loads in the practise list model that stores the list of selected names from
+     * the main menu to be practised.
+     * @param practiseListModel
+     */
     public void initModel(IPractiseListModel practiseListModel) {
         _practiseListModel = practiseListModel;
         _displayName.setText("Name: " + _practiseListModel.nextName().toString());
     }
 
+    /**
+     * Moves to the next Name in the list and updates the displayed name. The Name is
+     * unchanged if the end of the list is reached.
+     */
     public void nextButtonPressed() {
         if (_practiseListModel.hasNext()) {
             _displayName.setText("Name: " + _practiseListModel.nextName().toString());
@@ -50,6 +55,10 @@ public class PlaySceneController implements Initializable {
 
     }
 
+    /**
+     * Moves to the previous Name in the list and updates the displayed name. The Name
+     * is unchanged if there are no previous names.
+     */
     public void previousButtonPressed() {
         if (_practiseListModel.hasPrevious()) {
             _displayName.setText("Name: " + _practiseListModel.previousName().toString());
@@ -60,10 +69,14 @@ public class PlaySceneController implements Initializable {
         _practiseListModel.keepRecording();
     }
 
+    /**
+     * When the recording button is pressed the recording scene is displayed to the user
+     */
     public void recordButtonPressed() {
         Parent playerParent = null;
         FXMLLoader loader = new FXMLLoader();
 
+        // load in the recording scene
         try {
             loader.setLocation(getClass().getResource("/app/views/RecordingScene.fxml"));
             playerParent = loader.load();
@@ -71,34 +84,45 @@ public class PlaySceneController implements Initializable {
             e.printStackTrace();
         }
 
+
+        // pass the model to the recording scene controller
         RecordingSceneController controller = loader.getController();
         controller.initModel(_practiseListModel);
 
         Scene playerScene = new Scene(playerParent);
 
+        // switch scenes
         Stage window = new Stage();
         window.setScene(playerScene);
         window.initModality(Modality.APPLICATION_MODAL);
         window.showAndWait();
 
+        // enable buttons
         keep_btn.setDisable(false);
         compare_btn.setDisable(false);
 
     }
 
-
+    /**
+     * Plays the currently displayed name
+     */
     public void playButtonPressed() {
         _practiseListModel.playCurrentName();
     }
 
+    /**
+     * Redirects the user back to the main menu
+     */
     public void handleReturnAction(ActionEvent event) throws IOException {
+        // load in the main menu scene
         Parent playerParent = FXMLLoader.load(getClass().getResource("/app/views/NameSayer.fxml"));
         Scene playerScene = new Scene(playerParent);
 
+        // switch scenes
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(playerScene);
     }
-
+    
     public void compareButtonPressed() {
         _practiseListModel.compareUserRecording();
     }
