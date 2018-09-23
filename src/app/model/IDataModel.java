48bdc581d89d85e93e670caec86c8ff46ed1f101
Harrison Leach <HarrisonL7190@gmail.com>
2018-09-23 17:04:47 +1200

    Updated documentation

--- src/app/model/IDataModel.java
+++ src/app/model/IDataModel.java
@@ -1,9 +1,12 @@
 package app.model;
 
-import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.TreeItem;
-import sun.reflect.generics.tree.Tree;
 
+/**
+ * A data model represents the database in which practise and user recordings
+ * are to be stored. The displayable databases are returned in Tree View form
+ * which allows them to be easily presentable to the user.
+ */
 public interface IDataModel {
 
     TreeItem<Name> loadDatabase();
