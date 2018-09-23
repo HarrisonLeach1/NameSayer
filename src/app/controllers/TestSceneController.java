a6c81371063491e06ed2fc67f1f31011990e38ba
Harrison Leach <HarrisonL7190@gmail.com>
2018-09-23 20:02:35 +1200

    Added documentation to test scene controller

--- src/app/controllers/TestSceneController.java
+++ src/app/controllers/TestSceneController.java
@@ -1,5 +1,6 @@
 package app.controllers;
 
+import app.model.Name;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -24,10 +25,16 @@ public class TestSceneController implements Initializable {
     @FXML private Button _playBtn,_okBtn;
     @FXML private ProgressBar _progress;
 
+    public static final String TEST_FILE = ".test.wav";
+
+    /**
+     * When the test mic scene opens the users mic is recorded for five seconds.
+     * The progress bar is updated to indicate how much time is left for testing.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
-            String cmd = "mkdir -p " + USER_DATABASE + "; ffmpeg -y -f alsa -t " + RECORD_TIME + " -i default "+ USER_DATABASE + ".test.wav";
+            String cmd = "mkdir -p " + USER_DATABASE + "; ffmpeg -y -f alsa -t " + RECORD_TIME + " -i default "+ USER_DATABASE + TEST_FILE;
 
             ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
             builder.start();
@@ -35,6 +42,8 @@ public class TestSceneController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // create new timeline which updates the progress bar for 5 seconds
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.ZERO, new KeyValue(_progress.progressProperty(), 0)),
                 new KeyFrame(Duration.seconds(5), e -> {
@@ -44,22 +53,28 @@ public class TestSceneController implements Initializable {
         );
         timeline.setCycleCount(1);
         timeline.play();
-
     }
 
 
+    /**
+     * Deletes the test recording when the user exits the window.
+     * @param event
+     */
     public void handleReturnAction(ActionEvent event) {
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        File file = new File(USER_DATABASE + ".test.wav");
+        File file = new File(USER_DATABASE + TEST_FILE);
         if (file.exists()) {
             file.delete();
         }
         window.close();
     }
 
+    /**
+     * Plays back the test recording to the user.
+     */
     public void handlePlayAction(ActionEvent event) {
         try {
-            String cmd = "ffplay " + USER_DATABASE + ".test.wav -autoexit -nodisp";
+            String cmd = "ffplay " + USER_DATABASE + TEST_FILE + " -autoexit -nodisp";
 
             ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
             builder.start();
