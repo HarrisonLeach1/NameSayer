27e4bd8c0ae640a4b5ca6745cc97530b8a0fa5a9
Harrison Leach <HarrisonL7190@gmail.com>
2018-09-23 16:59:01 +1200

    Improved data model documentation

--- src/app/model/IDataModel.java
+++ src/app/model/IDataModel.java
@@ -6,8 +6,24 @@ import sun.reflect.generics.tree.Tree;
 
 public interface IDataModel {
 
+    /**
+     * Loads the database of recordings as a tree. The root is returned,
+     * all user recordings are descendants of the root. All names children of
+     * the root. If there is more than one version of a name, those version
+     * will be children of that name.
+     *
+     * @return the TreeItem root of the TreeView
+     */
     TreeItem<Name> loadDatabase();
 
+    /**
+     * Loads the database of user recordings as a tree. The root is returned,
+     * all user recordings are descendants of the root. All names children of
+     * the root. If there is more than one version of a name, those version
+     * will be children of that name.
+     *
+     * @return the TreeItem root of the TreeView
+     */
     TreeItem<Name> loadUserDatabase();
 
 }
