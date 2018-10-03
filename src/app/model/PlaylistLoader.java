db06aa4e379371b3b666a1efcb32e4d5df4c341b
Harrison Leach <HarrisonL7190@gmail.com>
2018-10-04 11:22:28 +1300

    Added load file method

--- src/app/model/PlaylistLoader.java
+++ src/app/model/PlaylistLoader.java
@@ -1,13 +1,16 @@
 package app.model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PlaylistLoader {
 
     private final List<ConcatenatedName> _names;
 
-    public PlaylistLoader(File playlistFile) {
+    public PlaylistLoader(File playlistFile) throws FileNotFoundException {
         _names = loadFile(playlistFile);
     }
 
@@ -15,8 +18,23 @@ public class PlaylistLoader {
         _names = loadSingleName(singleName);
     }
 
-    private List<ConcatenatedName> loadFile(File playlistFile) {
+    /**
+     * Given text file of names, returns a list of names where each name corresponds
+     * to a line of names in the given text file.
+     * @param playlistFile
+     * @return list of names
+     * @throws FileNotFoundException
+     */
+    private List<ConcatenatedName> loadFile(File playlistFile) throws FileNotFoundException {
+        Scanner input = new Scanner(playlistFile);
+        List<String> list = new ArrayList<String>();
 
+        // load in each line of the text file as a string
+        while (input.hasNextLine()) {
+            list.add(input.nextLine());
+        }
+
+        return stringsToNames(list);
     }
 
     private List<ConcatenatedName> loadSingleName(String singleName) {
