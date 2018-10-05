b3fb3afb641d0cbf84f2dd64d59e9502a8d8844e
Harrison Leach <HarrisonL7190@gmail.com>
2018-10-05 19:33:53 +1300

    Added writeUserData

--- src/app/model/UserModel.java
+++ src/app/model/UserModel.java
@@ -1,19 +1,22 @@
 package app.model;
 
+import java.awt.color.ICC_Profile;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class UserModel {
-    private static final String DATA_FILE = "userData.txt"
+    private static final File DATA_FILE = new File("userData.txt");
     private static final UserModel INSTANCE = new UserModel();
-    private int _streakCount;
+    private int _streakCount = 1;
     private LocalDate _lastLogin;
-    private int _userXP;
+    private int _userXP = 0;
 
-    private UserModel() throws FileNotFoundException {
+    private UserModel() {
         readUserData();
         updateStreak();
     }
@@ -34,20 +37,45 @@ public class UserModel {
         LocalDate today = LocalDate.now();
         LocalDate yesterday = today.minusDays(1);
         if (_lastLogin == yesterday) {
-
+            _streakCount++;
         }
         _lastLogin = today;
-        //writeUserData();
+        writeUserData();
     }
 
-    private void readUserData() throws FileNotFoundException {
-        Scanner input = new Scanner(new File(DATA_FILE));
+    private void writeUserData() {
+        File file = DATA_FILE;
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        _lastLogin = LocalDate.parse(input.nextLine(), formatter);
+        // create userData.txt if it does not already exist
+        try {
+            file.createNewFile();
 
-        _streakCount = Integer.parseInt(input.nextLine());
+            FileWriter fw = new FileWriter(DATA_FILE,false);
 
-        _userXP = Integer.parseInt(input.nextLine());
+            fw.write(_lastLogin + "\r\n");
+
+            fw.write(_streakCount + "\r\n");
+
+            fw.write(_userXP + "\r\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void readUserData() {
+        try {
+            Scanner input = new Scanner(DATA_FILE);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            _lastLogin = LocalDate.parse(input.nextLine(), formatter);
+
+            _streakCount = Integer.parseInt(input.nextLine());
+
+            _userXP = Integer.parseInt(input.nextLine());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
