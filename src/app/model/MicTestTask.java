adc2cdbf9856ec4f191ff2c5cb1c9c2e15ffa881
Harrison Leach <HarrisonL7190@gmail.com>
2018-10-17 12:43:07 +1300

    Added documentation and removed magic numbers from MicTestTask to increase readability

--- src/app/model/MicTestTask.java
+++ src/app/model/MicTestTask.java
@@ -1,7 +1,6 @@
 package app.model;
 
 import javafx.concurrent.Task;
-
 import javax.sound.sampled.*;
 
 /**
@@ -14,30 +13,38 @@ import javax.sound.sampled.*;
 public class MicTestTask extends Task {
     private static final AudioFormat AUDIO_FORMAT = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 2, 4, 44100, false);
     private static final DataLine.Info INFO = new DataLine.Info(TargetDataLine.class, AUDIO_FORMAT);
-    private TargetDataLine targetLine;
+    private static final double MAX_PROGRESS = 100;
+    private static final int BUFFER_DIVISION = 5;
+    private TargetDataLine _targetLine;
 
 
     public MicTestTask() {
         try {
-            targetLine = (TargetDataLine) AudioSystem.getLine(INFO);
+            _targetLine = (TargetDataLine) AudioSystem.getLine(INFO);
         } catch (LineUnavailableException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Updates the progress of this Task object based on the volume
+     * level of the input received from the user's mic.
+     * @return
+     */
     @Override
     protected Object call() {
         try {
-            //Microphone
-            targetLine.open();
-            targetLine.start();
+            // initialise mic input
+            _targetLine.open();
+            _targetLine.start();
 
-            byte[] data = new byte[targetLine.getBufferSize() / 5];
+            byte[] data = new byte[_targetLine.getBufferSize() / BUFFER_DIVISION];
 
+            // while this task is alive keep reading the users mic level and updating this Task appropriately
             while (true) {
-                targetLine.read(data, 0, data.length);
+                _targetLine.read(data, 0, data.length);
                 int level = calculateRMSLevel(data);
-                updateProgress(level,100);
+                updateProgress(level,MAX_PROGRESS);
             }
         } catch (LineUnavailableException lue) {
             lue.printStackTrace();
@@ -45,21 +52,31 @@ public class MicTestTask extends Task {
         return null;
     }
 
+    /**
+     * The task is cancelled, closing the line to the users microphone
+     * input and ending volume updates.
+     */
     @Override
     protected void cancelled() {
-        targetLine.stop();
-        targetLine.close();
+        _targetLine.stop();
+        _targetLine.close();
         super.cancelled();
     }
 
-
+    /**
+     * Updates the progress of this task which actually represents the volume
+     * level of the users input to the mic.
+     * @param workDone
+     * @param max
+     */
     @Override
     protected void updateProgress(double workDone, double max) {
         super.updateProgress(workDone, max);
     }
 
     /**
-     * Calculates the microphone input and turns it into an integer
+     * Calculates the microphone input RMS Level and turns it into an integer
+     * that can be used to display mic volume information to the user.
      */
     private int calculateRMSLevel(byte[] audioData) {
         long lSum = 0;
